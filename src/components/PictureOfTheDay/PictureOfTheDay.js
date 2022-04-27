91957edb3772fe3349dccbd644e3416eb2d3cab0
Boomerang24 <alexcomoed@gmail.com>
2022-04-27 01:04:06 -0500

    changed regular URL for HD URL

--- src/components/PictureOfTheDay/PictureOfTheDay.js
+++ src/components/PictureOfTheDay/PictureOfTheDay.js
@@ -3,12 +3,12 @@ import React from 'react';
 import MediaContainer from '../MediaContainer';
 
 const PictureOfTheDay = ({ todaysInfo }) => {
-  const { url, title, explanation: description, media_type } = todaysInfo;
+  const { hdurl, title, explanation: description, media_type } = todaysInfo;
   return (
     <div className="main-sections">
       <div className="picture-information">
         <h3>{title}</h3>
-        <MediaContainer mediaSource={url} alt={title} mediaType={media_type} />
+        <MediaContainer mediaSource={hdurl} alt={title} mediaType={media_type} />
       </div>
       <div className="picture-description">
         <p>{description}</p>
